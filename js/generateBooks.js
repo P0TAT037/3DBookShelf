17377d70b433fe99361351c1481b305173636d62
P0TAT037 <77874294+P0TAT037@users.noreply.github.com>
2022-01-08 21:48:42 +0200

    all kinda working as long as the pdf isn't too long

--- js/generateBooks.js
+++ js/generateBooks.js
@@ -1,35 +1,5 @@
-var pdf = pdfjsLib.getDocument('../res/SWE.pdf');
-renderFirstPage(pdf, "titleb1");
-document.getElementById("titleb1").addEventListener('click', turn());
-
-function renderFirstPage(pdf, canvId)
-{
-    pdf.promise.then(function(pdf){
-    pdf.getPage(1).then(function(page){
-            var scale = 1;
-            var viewport = page.getViewport({ scale: scale, });
-            // Support HiDPI-screens.
-            var outputScale = window.devicePixelRatio || 1;
-
-            var canvas = document.getElementById(canvId);
-            var context = canvas.getContext('2d');
-
-            canvas.width = Math.floor(viewport.width * outputScale);
-            canvas.height = Math.floor(viewport.height * outputScale);
-            canvas.style.width = 130 + "px";
-            canvas.style.height =  180 + "px";
-            
-            var transform = outputScale !== 1
-            ? [outputScale, 0, 0, outputScale, 0, 0]
-            : null;
-
-            var renderContext = {
-            canvasContext: context,
-            transform: transform,
-            viewport: viewport
-            };
-            page.render(renderContext);
-        })
-    });
-}
+var bookHtml = "";
+bookHtml += "<img class='bookImg' src='/res/THE EVENDER.jpg' id='b1' onclick=turn('../res/sample.pdf')>\n <div hidden class='book' id='../res/sample.pdf'></div>\n\n";
+bookHtml += "<img class='bookImg' src='/res/THE EVENDER.jpg' id='b2' onclick=turn('../res/SWE.pdf')>\n <div hidden class='book' id='../res/SWE.pdf'></div>\n\n";
+document.getElementById('lib').innerHTML= bookHtml;
 
